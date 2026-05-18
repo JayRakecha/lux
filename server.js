2273d5e57a1caf694f0a870e2aba49c08eb9d818
jay rakecha <jayrakecha12@gmail.comgit config --global user.name jay>
2026-05-18 15:35:17 +0530

    fix ssl

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const db = mysql.createPool({
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
-  ssl: process.env.MYSQLHOST ? { rejectUnauthorized: true } : false
+  ssl: process.env.MYSQLHOST ? { rejectUnauthorized: false } : false
 });
  
 
